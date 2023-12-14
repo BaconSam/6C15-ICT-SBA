0b602e9bf8a80cf584442c3fdcaf4ea34b3f9831
1A21SamSam <2158069-1A21SamSam@users.noreply.replit.com>
2023-12-14 14:19:25 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,46 @@
+def inputHouse(corr):
+    while corr == False:
+        Ihouse = str(input("input the house of the student. (R/G/B/Y) or input \"end\" to stop inputting  ")).lower()
+        
+        match Ihouse:
+            case "r":
+                Ihouse = "Red"
+                corr = True
+            case "g":
+                Ihouse = "Green"
+                corr = True
+            case "b":
+                Ihouse = "Blue"
+                corr = True
+            case "y":
+                Ihouse = "Yellow"
+                corr = True
+            case "quit"|"end":
+                Ihouse = "end"
+                corr = True
+                return Ihouse
+            case _:
+                print("Please input accordingly\nRed House: R\nGreen House: G\nBlue House: B\nYellow House: Y")
+                corr = False
+
+    print("The student is " + Ihouse)
+    return Ihouse
+
+def inputSeed():
+    IsBool = False
+    seed = "nil"
+    while IsBool == False:
+        seed = input("Is he/she a seed player? T/F  ").lower()
+
+        match seed:
+            case "t":
+                seed = True
+                IsBool = True
+            case "f":
+                seed = False
+                IsBool = True
+            case _:
+                print("Enter a boolean value\nTrue: T / t\nFalse: F / f") 
+    
+    print("Seed: " + str(seed))
+    return seed
---
+++ main.py
@@ -0,0 +1,33 @@
+from items import Item
+from functions import *
+import csv, json
+import PySimpleGUI as sg
+
+sg.theme('Dark2')
+
+
+
+def main():
+    list_item = []
+    Ihouse = "-1"
+    seed = "nil"
+
+    for i in range(16):
+        # House
+        corr_info = False
+        Ihouse = inputHouse(corr_info)
+
+        if Ihouse == "end":
+            return
+
+        # Seed
+        seed = inputSeed()
+
+        # Store in item
+        print(str(i))
+        list_item.append(Item(Ihouse, seed, False))
+        print(f"Done {list_item[i].House} {list_item[i].Seed}")
+
+
+if __name__ == "__main__":
+    main()
